7d44444ffa07d095e1f96156a9a6c8db4146b1ea
Tim Barrass <faveflave@gmail.com>
2013-03-21 15:50:03 +1100

    agh

--- doc/html/search/all_77.js
+++ doc/html/search/all_77.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['waveshape_5fsigmoid_5fnum_5fcells',['WAVESHAPE_SIGMOID_NUM_CELLS',['../group__tables.html#ga4781d8e558797b58eccc12d3d8dc52f1',1,'waveshape_sigmoid_int8.h']]],
   ['waveshaper',['WaveShaper',['../class_wave_shaper.html',1,'WaveShaper&lt; T &gt;'],['../class_wave_shaper_3_01char_01_4.html#a0516cf5c7a3b827b1822480b46af132a',1,'WaveShaper&lt; char &gt;::WaveShaper()'],['../class_wave_shaper_3_01int_01_4.html#a525265947dad0fc29c2c020d2620fcc3',1,'WaveShaper&lt; int &gt;::WaveShaper()']]],
   ['waveshaper_3c_20char_20_3e',['WaveShaper< char >',['../class_wave_shaper_3_01char_01_4.html',1,'']]],
   ['waveshaper_3c_20int_20_3e',['WaveShaper< int >',['../class_wave_shaper_3_01int_01_4.html',1,'']]],

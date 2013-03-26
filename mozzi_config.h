b9cd3f6a493da2f542e9986f689c9099d5ce9aee
Tim Barrass <faveflave@gmail.com>
2013-03-27 01:01:53 +1100

    fixed missing config

---
+++ mozzi_config.h
@@ -0,0 +1,19 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+
+/**  @ingroup core
+Select STANDARD or HIFI audio output mode in the Mozzi/config.h file with either
+\#define AUDIO_MODE STANDARD
+or 
+\#define AUDIO_MODE HIFI
+In Mozzi/config.h, comment one of these options in and the other out to set the audio mode.
+*/
+
+#define AUDIO_MODE STANDARD
+//#define AUDIO_MODE HIFI
+
+
+
+#endif        //  #ifndef CONFIG_H
+

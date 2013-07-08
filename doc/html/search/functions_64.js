579fe81ae807a08d729b0d12bf6cb62fcbb1f91c
Tim Barrass <faveflave@gmail.com>
2013-07-09 02:07:37 +1000

    Site updated at 2013-07-08 16:07:37 UTC

--- doc/html/search/functions_64.js
+++ doc/html/search/functions_64.js
@@ -1,7 +1,5 @@
 var searchData=
 [
   ['dcfilter',['DCfilter',['../class_d_cfilter.html#ab55e871fc9d11dfb9231e44627181c2c',1,'DCfilter']]],
-  ['disconnectdigitalin',['disconnectDigitalIn',['../group__analog.html#gad4433797b75fedd473250e9aa414dcae',1,'disconnectDigitalIn(byte channel_num):&#160;mozzi_analog.cpp'],['../group__analog.html#gad4433797b75fedd473250e9aa414dcae',1,'disconnectDigitalIn(byte channel_num):&#160;mozzi_analog.cpp']]],
-  ['dummy',['dummy',['../_mozzi_guts_8cpp.html#a100d09f9a57d44745299c28c63c98745',1,'MozziGuts.cpp']]],
-  ['dummy_5ffunction',['dummy_function',['../_mozzi_guts_8cpp.html#ac0d4c2a99f2e90d864a74293d935e857',1,'MozziGuts.cpp']]]
+  ['disconnectdigitalin',['disconnectDigitalIn',['../group__analog.html#gad4433797b75fedd473250e9aa414dcae',1,'disconnectDigitalIn(byte channel_num):&#160;mozzi_analog.cpp'],['../group__analog.html#gad4433797b75fedd473250e9aa414dcae',1,'disconnectDigitalIn(byte channel_num):&#160;mozzi_analog.cpp']]]
 ];

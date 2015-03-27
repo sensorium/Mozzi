d0dbeab3e0ae0284b56096dacc6a71bda84cfe24
Alvaro [Andor] <andor@pierdelacabeza.com>
2015-03-27 21:54:21 +0100

    Correct case on table include
    Case sensitive filesystems (as on Linux) won't find COS2048_int8.h, but will work with cos2048_int8.h

--- examples/06.Synthesis/AMsynth/AMsynth.ino
+++ examples/06.Synthesis/AMsynth/AMsynth.ino
@@ -20,7 +20,7 @@
 //#include <ADC.h>  // Teensy 3.0/3.1 uncomment this line and install http://github.com/pedvide/ADC
 #include <MozziGuts.h>
 #include <Oscil.h>
-#include <tables/COS2048_int8.h> // table for Oscils to play
+#include <tables/cos2048_int8.h> // table for Oscils to play
 //#include <mozzi_utils.h>
 #include <mozzi_fixmath.h>
 #include <EventDelay.h>

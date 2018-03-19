12be2332827a9ec686b434fbcc29901ec2f75985
Adrian Freed <adrian@adrianfreed.com>
2018-03-19 10:24:07 -0700

    Terminated comment

--- AudioConfigSAMD21.h
+++ AudioConfigSAMD21.h
@@ -4,7 +4,7 @@
 
 /** @ingroup core
 */
-/* Used internally to put the 0-biased generated audio into the centre of the output range (12 bits)
+/* Used internally to put the 0-biased generated audio into the centre of the output range (12 bits) */
 #define AUDIO_BIAS ((uint16_t) 2048)
 
 

46e462acf4058fb39a6eebe676c0b52675a23dbb
mr.sensorium <faveflave@gmail.com>
2019-07-22 21:35:39 +1000

    default AUDIO_MODE STANDARD_PLUS again

--- mozzi_config.h
+++ mozzi_config.h
@@ -23,8 +23,8 @@ and comment out \#define AUDIO_MODE STANDARD and \#define AUDIO_MODE STANDARD_PL
 
 @note Teensy 3.* plays 12 bit audio in STANDARD or STANDARD_PLUS modes, and has no HIFI mode.
 */
-#define AUDIO_MODE STANDARD
-//#define AUDIO_MODE STANDARD_PLUS
+//#define AUDIO_MODE STANDARD
+#define AUDIO_MODE STANDARD_PLUS
 //#define AUDIO_MODE HIFI
 
 

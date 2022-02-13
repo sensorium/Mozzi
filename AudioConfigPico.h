57f7a5fd90fabba98626e7cb324551c9cbe61b59
j-enns <38088930+j-enns@users.noreply.github.com>
2022-02-13 11:29:52 -0700

    more bit/audio rate options
    default to 11 bit phase corrected audio

--- AudioConfigPico.h
+++ AudioConfigPico.h
@@ -3,8 +3,23 @@
 
 #include "hardware/pwm.h"
 
-#define AUDIO_BITS 12
-#define AUDIO_BIAS ((uint16_t) 1<<(AUDIO_BITS-1))
+
+/* use with PWM_RATE 65536 and phase corrected */
+// #define AUDIO_BITS 10 // closer to 9.986
+// #define AUDIO_BIAS 507
+// #define PHASE_CORRECT 1
+// #define AUDIO_RATE 65536         
+
+/* use with default AUDIO_RATE 32768 and PHASE_CORRECT or #define AUDIO_RATE 65536 */
+ #define AUDIO_BITS 11 // closer to 10.987
+ #define AUDIO_BIAS 1015
+ #define PHASE_CORRECT 1          // uncomment for phase corrected PWM
+// #define AUDIO_RATE 65536         // OR uncomment for faster audio rate
+
+/* use with PWM_RATE 32768 and not phase corrected */
+// #define AUDIO_BITS 12 // closer to 11.987
+// #define AUDIO_BIAS 2030
+// #define PHASE_CORRECT 0
 
 #define AUDIO_CHANNEL_1_PIN 2
 #define AUDIO_CHANNEL_2_PIN 3

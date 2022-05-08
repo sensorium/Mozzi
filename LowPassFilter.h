3c52a2f1948b994c8812976c480b5eae0dc4ba06
tomcombriat <tomcombriat@live.fr>
2022-05-08 21:22:34 +0200

    More constant resonance over whole range of LPF

--- LowPassFilter.h
+++ LowPassFilter.h
@@ -68,7 +68,7 @@ public:
   void setCutoffFreq(su cutoff)
       {
     f = cutoff;
-    fb = q + ucfxmul(q, SHIFTED_1 - cutoff);
+    fb = q + ucfxmul(q, SHIFTED_1 + cutoff);
   }
 
   /** deprecated.  Use setCutoffFreqAndResonance(su cutoff, su
@@ -93,7 +93,7 @@ public:
     f = cutoff;
     q = resonance; // hopefully optimised away when compiled, just here for
                    // backwards compatibility
-    fb = q + ucfxmul(q, SHIFTED_1 - cutoff);
+    fb = q + ucfxmul(q, SHIFTED_1 + cutoff);
   }
 
   /** Calculate the next sample, given an input signal.

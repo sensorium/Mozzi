c1a3d1c4f75c17039fb0ddd76564d58f8b0c3e17
tomcombriat <tomcombriat@live.fr>
2023-05-28 17:25:58 +0200

    First try at branching R4 output straight on the buffer data
    Set the buffer size as a define for easy future changes
    
    First try at hooking mozzi buffers with in-built analog generation
    Untested
    
    More low-level implementation for the dac, just an IRQ conflict remains

--- AudioConfigArduinoFSP.h
+++ AudioConfigArduinoFSP.h
@@ -8,16 +8,18 @@
 
 
 
-#define AUDIO_CHANNEL_1_PIN 9
+#define AUDIO_CHANNEL_1_PIN A0
 #if (AUDIO_CHANNELS > 1)
 #define AUDIO_CHANNEL_2_PIN 10
 #endif
 
-#define AUDIO_BITS 8 // random value for now
+#define AUDIO_BITS 12 // random value for now
 
 #define AUDIO_BITS_PER_CHANNEL AUDIO_BITS
 
 #define AUDIO_BIAS ((uint16_t) 1<<(AUDIO_BITS-1))
+
+#define BYPASS_MOZZI_OUTPUT_BUFFER true  // Mozzi initial buffer are not of the good type, so we bypass it and create our own
 
 #endif        //  #ifndef AUDIOCONFIGARDUINOFSP_H
 
--- MozziGuts_impl_ARDUINOFSP.hpp
+++ MozziGuts_impl_ARDUINOFSP.hpp
@@ -54,9 +54,9 @@ void setupMozziADC(int8_t speed) {
 
 /* NOTE: Most platforms call a specific function/ISR when conversion is complete. Provide this function, here.
  * From inside its body, simply call advanceADCStep(). E.g.:
-void stm32_adc_eoc_handler() {
-  advanceADCStep();
-}
+ void stm32_adc_eoc_handler() {
+ advanceADCStep();
+ }
 */
 ////// END analog input code ////////
 
@@ -65,59 +65,150 @@ void stm32_adc_eoc_handler() {
 //// BEGIN AUDIO OUTPUT code ///////
 
 
+FspTimer timer;
+
 #if (EXTERNAL_AUDIO_OUTPUT != true) // otherwise, the last stage - audioOutput() - will be provided by the user
-inline void audioOutput(const AudioOutput f) {
-  /*
-  analogWrite(AUDIO_CHANNEL_1_PIN, f.l()+AUDIO_BIAS);
-#if (AUDIO_CHANNELS > 1)
-  analogWrite(AUDIO_CHANNEL_2_PIN, f.r()+AUDIO_BIAS);
-  #endif*/
+#include <dac.h>
+#include <FspTimer.h>
+#include <r_dtc.h>
+
+
+volatile uint32_t pin;
+CircularBuffer<uint16_t> output_buffer;
+uint8_t dac_bits;
+dtc_instance_ctrl_t dtc_ctrl;
+transfer_info_t dtc_info;
+dtc_extended_cfg_t dtc_cfg_extend;
+transfer_cfg_t dtc_cfg;
+
+////////////// DAC CREATION AND FIRST INIT ///////////////////
+void dac_creation(pin_size_t pinN) {
+  if (IS_DAC(pinN)) {
+    auto cfg_dac = getPinCfgs(pinN, PIN_CFG_REQ_DAC);
+    pin = cfg_dac[0];
+    if (IS_DAC_8BIT(pin)) {
+      dac_bits = 8;
+      dtc_info.transfer_settings_word_b.size = TRANSFER_SIZE_1_BYTE;
+      if (GET_CHANNEL(pin) == 0) {
+#ifdef DAC_ADDRESS_8_CH0
+        dtc_info.p_dest = (void *)DAC_ADDRESS_8_CH0;
+#endif
+      } else if (GET_CHANNEL(pin) == 1) {
+#ifdef DAC_ADDRESS_8_CH1
+        dtc_info.p_dest = (void *)DAC_ADDRESS_8_CH1;
+#endif
+      }
+    } else {
+      dac_bits = 12;
+      dtc_info.transfer_settings_word_b.size = TRANSFER_SIZE_2_BYTE;
+      if (GET_CHANNEL(pin) == 0) {
+#ifdef DAC_ADDRESS_12_CH0
+        dtc_info.p_dest = (void *)DAC_ADDRESS_12_CH0;
+#endif
+      } else if (GET_CHANNEL(pin) == 1) {
+#ifdef DAC_ADDRESS_12_CH1
+        dtc_info.p_dest = (void *)DAC_ADDRESS_12_CH1;
+#endif
+      }
+    }
+  }
+
+  dtc_info.transfer_settings_word_b.dest_addr_mode = TRANSFER_ADDR_MODE_FIXED;
+  dtc_info.transfer_settings_word_b.repeat_area = TRANSFER_REPEAT_AREA_SOURCE;
+  dtc_info.transfer_settings_word_b.irq = TRANSFER_IRQ_END;
+  dtc_info.transfer_settings_word_b.chain_mode = TRANSFER_CHAIN_MODE_DISABLED;
+  dtc_info.transfer_settings_word_b.src_addr_mode =
+    TRANSFER_ADDR_MODE_INCREMENTED;
+  dtc_info.transfer_settings_word_b.mode = TRANSFER_MODE_REPEAT;
+  dtc_info.p_src = (void const *)NULL;
+
+  dtc_info.num_blocks = 0;
+  dtc_info.length = 0;
+
+  dtc_cfg.p_info = &dtc_info;
+  dtc_cfg.p_extend = &dtc_cfg_extend;
+
+  dtc_cfg_extend.activation_source = FSP_INVALID_VECTOR;
 }
-#else  // manually setting a timer for EXTERNAL_AUDIO_OUTPUT
-FspTimer timer1;
-void timer_callbar_dummy(timer_callback_args_t __attribute__((unused)) *args){defaultAudioOutput();};
+
+void dac_init() {
+  if (IS_DAC_8BIT(pin)) {
+#if DAC8_HOWMANY > 0
+    if (GET_CHANNEL(pin) < DAC8_HOWMANY) {
+      _dac8[GET_CHANNEL(pin)].init();
+    }
+#endif
+  } else {
+    if (GET_CHANNEL(pin) < DAC12_HOWMANY) {
+      _dac12[GET_CHANNEL(pin)].init();
+    }
+  }
+}
+
 #endif
 
-static void startAudio() {
 
-  //pinMode(AUDIO_CHANNEL_1_PIN,OUTPUT);
+//////////////// TIMER ////////////////
 
-#if (EXTERNAL_AUDIO_OUTPUT == true)
-  timer1.set_period(1.0 / AUDIO_RATE);
-  timer1.set_irq_callback(timer_callbar_dummy);
+#if EXTERNAL_AUDIO_OUTPUT == true
+void timer_callback_dummy(timer_callback_args_t __attribute__((unused)) *args){defaultAudioOutput();};
+#else
+void timer_callback_dummy(timer_callback_args_t __attribute__((unused)) *args){
+
+  output_buffer.read();
+  digitalWrite(LED_BUILTIN,HIGH);
 
 
-  uint8_t type=0;
+}; // to empty the buffer (the dac does not take care of it)
+#endif
+
+void timer_init() {
+  uint8_t type = 0;
   int8_t tindex = FspTimer::get_available_timer(type);
 
-  if (tindex < 0) tindex = FspTimer::get_available_timer(type, true);
-    
-  if (tindex >= 0) {
-    // defaultAudioOutput cannot be used straight, so we are using a dummy, with no argument, linking to defaultAudioOutput
-    timer1.begin(TIMER_MODE_PERIODIC, type, tindex,AUDIO_RATE, 50.0f, timer_callbar_dummy);
-    timer1.setup_overflow_irq();
-    timer1.open();
-    timer1.start();
+  if (tindex < 0) {
+    tindex = FspTimer::get_available_timer(type, true);
   }
-#else
 
+  if (tindex >= 0) {
+#if EXTERNAL_AUDIO_OUTPUT != true
+    FspTimer::force_use_of_pwm_reserved_timer();
+#endif
+    timer.begin(TIMER_MODE_PERIODIC, type, tindex, AUDIO_RATE, 50.0,timer_callback_dummy);
+    timer.setup_overflow_irq();
+#if EXTERNAL_AUDIO_OUTPUT != true
+     dtc_cfg_extend.activation_source = timer.get_cfg()->cycle_end_irq;
+#endif
+    timer.open();
+  }
+}
+
+
+inline void audioOutput(const AudioOutput f) {
+  output_buffer.write(f+AUDIO_BIAS);
+}
+#define canBufferAudioOutput() (!output_buffer.isFull())
+
+
+static void startAudio() {
+#if EXTERNAL_AUDIO_OUTPUT != true
+  dac_creation(AUDIO_CHANNEL_1_PIN);
+#endif
+  timer_init();
+#if EXTERNAL_AUDIO_OUTPUT != true
+  dac_init();
+  R_DTC_Open(&dtc_ctrl, &dtc_cfg);
+  R_DTC_Enable(&dtc_ctrl);
+
+  dtc_cfg.p_info->p_src = output_buffer.address();
+  dtc_cfg.p_info->length = MOZZI_BUFFER_SIZE;
+  R_DTC_Reconfigure(&dtc_ctrl, dtc_cfg.p_info);
+#endif
+  timer.start();
   
-  //Serial.println("sa");
-  //pinMode(AUDIO_CHANNEL_1_PIN,OUTPUT);
-
-  // Add here code to get audio output going. This usually involves:
-  // 1) setting up some DAC mechanism (e.g. setting up a PWM pin with appropriate resolution
-  // 2a) setting up a timer to call defaultAudioOutput() at AUDIO_RATE
-  // OR 2b) setting up a buffered output queue such as I2S (see ESP32 / ESP8266 for examples for this setup)
-  // remember that the user may configure EXTERNAL_AUDIO_OUTPUT, in which case, you'll want to provide step 2a), and only that.
-  #endif
 }
 
 void stopMozzi() {
-#if (EXTERNAL_AUDIO_OUTPUT)
-  timer1.stop();
-#else
-
-#endif
+  timer.stop();
 }
 //// END AUDIO OUTPUT code ///////

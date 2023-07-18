142c125a071e178eed2367195691ba96b0843ef8
mr.sensorium <faveflave@gmail.com>
2023-07-18 23:24:28 +1000

    tidy

--- extras/python/table_generator_template.py
+++ extras/python/table_generator_template.py
@@ -1,6 +1,7 @@
 import array
 import os
 import textwrap
+import random
 
 def generate(outfile, tablename, tablelength, samplerate):
     fout = open(os.path.expanduser(outfile), "w")
@@ -12,18 +13,18 @@ def generate(outfile, tablename, tablelength, samplerate):
     fout.write('#include "WProgram.h"'+'\n')
     fout.write('#endif'+'\n')
     fout.write('#include "mozzi_pgmspace.h"'+'\n \n')
-    fout.write('#define ' + tablename + '_NUM_CELLS '+ str(len(values))+'\n')
+    fout.write('#define ' + tablename + '_NUM_CELLS '+ str(tablelength)+'\n')
     fout.write('#define ' + tablename + '_SAMPLERATE '+ str(samplerate)+'\n \n')
     outstring = 'CONSTTABLE_STORAGE(int8_t) ' + tablename + '_DATA [] = {'
     try:
         for num in range(tablelength):
-            outstring += str(num/32) + ", "  ## put your own generating code here
+            outstring += str(random.randint(-128, 127)) + ", "  ## put your own generating code here
     finally:
         outstring +=  "};"
         outstring = textwrap.fill(outstring, 80)
         fout.write(outstring)
         fout.write('\n \n #endif /* ' + tablename + '_H_ */\n')
         fout.close()
-        print "wrote " + outfile
+        print("wrote " + outfile)
 
-generate("~/Desktop/phasor8192_uint8.h", "phasor8192", 8192, "8192")
+generate("~/Desktop/whitenoise_4096_3_int8.h", "WHITENOISE_4096_3", 4096, 16384) # adjust to suit

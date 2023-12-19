11545ddd74078d41be6d00907678a64f43e5b343
tomcombriat <tomcombriat@live.fr>
2023-12-19 22:16:33 +0100

    Implemented mozzi_midi for FixMath

--- mozzi_midi.cpp
+++ mozzi_midi.cpp
@@ -167,14 +167,3 @@ int mtof(int midi_note){
 	return (FLASH_OR_RAM_READ<const uint32_t>(midiToFreq + midi_note) >> 16);
 }
 
-inline UFixMath<16,16> mtof(UFixMath<16,16> midival)
-{
-  return Q16n16_mtof(midival.asRaw());
-}
-
-
-template<byte NI, byte NF>
- inline UFixMath<16,16> mtof(UFixMath<NI,NF> midival)
-{
-  return Q16n16_mtof(UFixMath<16,16>(midival).asRaw());
-}
--- mozzi_midi.h
+++ mozzi_midi.h
@@ -8,9 +8,22 @@ float mtof(float x);
 int mtof(uint8_t midi_note);
 int mtof(int midi_note);
 Q16n16 Q16n16_mtof(Q16n16 midival);
-UFixMath<16,16> mtof(UFixMath<16,16> midival);
+
+inline UFixMath<16,16> mtof(UFixMath<16,16> midival)
+{
+  return Q16n16_mtof(midival.asRaw());
+}
 
 template<byte NI, byte NF>
-  UFixMath<16,16> mtof(UFixMath<NI,NF> midival);
+  inline UFixMath<16,16> mtof(UFixMath<NI,NF> midival)
+{
+  return Q16n16_mtof(UFixMath<16,16>(midival).asRaw());
+}
+
+template<byte NI, byte NF>
+  inline UFixMath<16,16> mtof(SFixMath<NI,NF> midival)
+{
+  return Q16n16_mtof(UFixMath<16,16>(midival).asRaw());
+}
 
 #endif /* MOZZI_MIDI_H_ */

862954fd269675c4ebb5dc7b7a82e42630b075dd
tomcombriat <tomcombriat@live.fr>
2023-12-24 15:47:24 +0100

    Fixed mozzi_midi.h with FixMath

--- mozzi_midi.h
+++ mozzi_midi.h
@@ -11,19 +11,19 @@ Q16n16 Q16n16_mtof(Q16n16 midival);
 
 inline UFixMath<16,16> mtof(UFixMath<16,16> midival)
 {
-  return Q16n16_mtof(midival.asRaw());
+  return UFixMath<16,16>::fromRaw(Q16n16_mtof(midival.asRaw()));
 }
 
 template<byte NI, byte NF>
   inline UFixMath<16,16> mtof(UFixMath<NI,NF> midival)
 {
-  return Q16n16_mtof(UFixMath<16,16>(midival).asRaw());
+  return UFixMath<16,16>::fromRaw(Q16n16_mtof(UFixMath<16,16>(midival).asRaw()));
 }
 
 template<byte NI, byte NF>
   inline UFixMath<16,16> mtof(SFixMath<NI,NF> midival)
 {
-  return Q16n16_mtof(UFixMath<16,16>(midival).asRaw());
+  return UFixMath<16,16>::fromRaw(Q16n16_mtof(UFixMath<16,16>(midival).asRaw()));
 }
 
 #endif /* MOZZI_MIDI_H_ */
